1325402e11f3c6a2ef4b902b592c38d70d42d8ef
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-07-04 12:09:00 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -7,3 +7,33 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   generateSoundex("AX", soundex);
  // ASSERT_EQ(soundex,"A200");
 }
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_02) {
+ //AAA
+  char soundex[5];
+  generateSoundex("Rubin", soundex);
+  ASSERT_EQ(soundex,"R150");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_03) {
+ //AAA
+  char soundex[5];
+  generateSoundex("Bob", soundex);
+  ASSERT_EQ(soundex,"B100");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_04) {
+ //AAA
+  char soundex[5];
+  generateSoundex("Z", soundex);
+  ASSERT_EQ(soundex,"Z000");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_05) {
+ //AAA
+  char soundex[5];
+  generateSoundex("A", soundex);
+  ASSERT_EQ(soundex,"A000");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_06) {
+ //AAA
+  char soundex[5];
+  generateSoundex("$", soundex);
+  ASSERT_EQ(soundex,"0000");
+}
